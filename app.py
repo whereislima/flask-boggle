69db97a733c50b3a0b53e8c99f23d0cf9eab219c
whereislima <reach.lima@gmail.com>
2021-04-11 20:39:52 -0700

    completed with solution

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template, redirect, jsonify, session
+from flask import Flask, request, render_template, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
+
+from boggle import Boggle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mangopudding"
 debug = DebugToolbarExtension(app)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"]=False
 
-from boggle import Boggle
 
 boggle_game = Boggle()
 
@@ -14,23 +15,32 @@ boggle_game = Boggle()
 def homepage():
     """ show game board """
     board = boggle_game.make_board()
-
     session["board"] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
 
-    return render_template('boggle.html', board=board)
+    return render_template('boggle.html', board=board, highscore=highscore,
+                           nplays=nplays)
 
 
 @app.route('/check-word')
 def check_word():
 
-  word = request.form["word"]
-  board = session["board"]
-  response = boggle_game.check_valid_word(board, word)
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
 
-  print("******************** valid **********************")
-  print(response)
-  print("******************** valid **********************")
-
-  return jsonify({'result': response})
+    return jsonify({'result': response})
 
 
+@app.route("/post-score", methods=["POST"])
+def post_score():
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
